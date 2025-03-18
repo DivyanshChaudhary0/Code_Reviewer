2a2675e0dbf4d87d47b80fe583b8731556ded839
Divyansh chaudhary <hypr123890@gmail.com>
2025-03-18 19:06:25 +0530

    make socket connection online

--- Frontend/src/utils/Socket.jsx
+++ Frontend/src/utils/Socket.jsx
@@ -1,12 +1,17 @@
 
 import { io } from "socket.io-client";
 
-export const BASE_URL = "http://localhost:4000"
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:4000" : "http://3.110.28.69/api"
 
 const socketConnection = (projectId) => {
-    return io("http://localhost:4000", {
-        query: { projectId , token: localStorage.getItem("token")},
+    return io(BASE_URL, {
+        path: "/api/socket.io/",  // Correct WebSocket path
+        query: {
+            projectId,
+            token: localStorage.getItem("token"),
+        },
+        transports: ["websocket", "polling"], // Ensures WebSockets work properly
     });
-}
+};
 
 export default socketConnection;
